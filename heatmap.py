32724d06795b063f64023ec6384cd27768a07749
Gary Belvin <gdb@google.com>
2019-05-31 22:36:38 +0100

    Draw force boundaries

--- heatmap.py
+++ heatmap.py
@@ -3,18 +3,25 @@
 import folium
 import os
 
-print(folium.__version__)
-
-lon, lat = -38.625, -12.875
-
-zoom_start = 8
+'''
+# Convert KML to geojson
+import kml2geojson
+import glob
+for path in glob.glob('police_data/force_kmls/*.kml'):
+    kml2geojson.main.convert(path, 'police_data/force_geo/')
+'''
 
 m = folium.Map(
-    location=[lat, lon],
-    tiles='OpenStreetMap',
-    zoom_start=zoom_start,
+    location=[51.5074, 0.1278],
+    zoom_start=12,
     control_scale=True
 )
+
+force_geo = 'police_data/force_geo'
+folium.GeoJson(f'{force_geo}/metropolitan.geojson', name='metro').add_to(m)
+folium.GeoJson(f'{force_geo}/city-of-london.geojson', name='city').add_to(m)
+
+folium.LayerControl().add_to(m)
 
 m.save(os.path.join('results', 'ControlScale.html'))
 

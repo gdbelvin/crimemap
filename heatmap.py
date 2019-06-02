7750c2a4c8fd905ebf150183937118943d2ae4d3
Gary Belvin <gdb@google.com>
2019-06-02 15:00:16 +0100

    Crime heatmap

--- heatmap.py
+++ heatmap.py
@@ -1,25 +1,67 @@
 #!/usr/bin/env python3
 
 import folium
+from folium import plugins
 import os
+import glob
+import pandas as pd
 
 '''
 # Convert KML to geojson
 import kml2geojson
-import glob
-for path in glob.glob('police_data/force_kmls/*.kml'):
+for path in glob.glob('police_data/boundaries/force_kmls/*.kml'):
     kml2geojson.main.convert(path, 'police_data/force_geo/')
 '''
 
 m = folium.Map(
-    location=[51.5074, 0.1278],
-    zoom_start=12,
+    location=[51.517742, -0.187590],
+    zoom_start=14,
     control_scale=True
 )
 
-force_geo = 'police_data/force_geo'
-folium.GeoJson(f'{force_geo}/metropolitan.geojson', name='metro').add_to(m)
-folium.GeoJson(f'{force_geo}/city-of-london.geojson', name='city').add_to(m)
+# Police force boundaries
+if False :
+    force_geo = 'police_data/boundaries/force_geo'
+    folium.GeoJson(f'{force_geo}/metropolitan.geojson', name='metro').add_to(m)
+    folium.GeoJson(f'{force_geo}/city-of-london.geojson', name='city').add_to(m)
+
+
+if False:
+    # neighborhood Boundaries
+    # https://data.police.uk/data/boundaries/
+    neighborood_geo = 'police_data/boundaries/neighborhood/2019-03'
+    for path in glob.glob(f'{neighborood_geo}/*/*.geojson'):
+        name = os.path.basename(path)
+        folium.GeoJson(path,
+            name=name,
+            tooltip=folium.features.Tooltip(name, sticky=False),
+        ).add_to(m)
+
+
+# Crime HeatMap
+if True:
+    crime_data = 'police_data/crime/2019-03-metropolitan-street.csv'
+    df_crime = pd.read_csv(crime_data)
+    df_crime = df_crime.dropna(subset=['Latitude', 'Longitude'])
+    # df_crime = df_crime[df_crime['LSOA code'].isin(['E05000631'])]
+    crime_arr = df_crime[['Latitude', 'Longitude']].values
+    m.add_child(plugins.HeatMap(crime_arr, radius=12))
+
+
+# Crime Points
+if False:
+    #df_crime = pd.DataFrame(df_crime, columns = ['Latitude','Longitude', 'LSOA code', 'Crime type'])
+    crime_layer = folium.FeatureGroup(name = 'Crime Locations')
+    for i, row in df_crime.iterrows():
+        crime_layer.add_child(
+            folium.CircleMarker([row['Latitude'], row['Longitude']],
+                            popup=row['Crime type'],
+                            radius=5,
+                            fill_color="#3db7e4", # divvy color
+                            ) )
+        if i > 100000:
+            break
+    m.add_child(crime_layer)
 
 folium.LayerControl().add_to(m)
 
